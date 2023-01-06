f0e83f53d5324dc4aee977c0e6de8a93da40d4ac
Matheus Rocha <matheusmartinsInsper>
2023-01-06 17:43:45 -0300

    sem livros repetidos

--- main.js
+++ main.js
@@ -8,18 +8,40 @@ function Cadastrar() {
     Preco: `${document.getElementById("preco").value}-R$`,
     id: makeId(),
   };
+  // if (livro.Nome == Livros.find(({ Nome }) => Nome == livro.Nome)) {
+  //   console.log("deu bom");
+  // } else {
+  //   console.log("deu ruim");
+  // }
   if (validarLivro(livro) == true) {
     //chamar a função que verifica a repetição aqui
+    const nomes = Livros.map(({ Nome }) => Nome);
+    const anos = Livros.map(({ Ano }) => Ano);
+    const categorias = Livros.map(({ Categoria }) => Categoria);
+    const precos = Livros.map(({ Preco }) => Preco);
 
-    salvarLivros(livro);
+    if (
+      livro.Nome == nomes.find((element) => element == livro.Nome) &&
+      livro.Ano == anos.find((element) => element == livro.Ano)
+    ) {
+      alert("livro ja cadastrado");
+    } else {
+      salvarLivros(livro);
+    }
   } else {
     return;
   }
 }
+
 function salvarLivros(element) {
   Livros.push(element);
   console.log(Livros);
+  document.getElementById("nome").value = "";
+  document.getElementById("ano").value = "";
+  document.getElementById("categoria").value = "";
+  document.getElementById("preco").value = "";
 }
+
 function validarLivro(Livro) {
   if (Livro.Nome == "") {
     alert("Insira o nome do Livro");
@@ -36,17 +58,19 @@ function validarLivro(Livro) {
   }
   return true;
 }
+
 let makeId = () => {
   let number = Math.round(Math.random().toFixed(7) * 10000000);
   return number;
 };
-let repeticaoLivros = (Livro) => {
-  for (let i = 0; i < Livros.length; i++) {
-    Livro.Nome == Livros[i].Nome &&
-    Livro.Ano == Livros[i].Ano &&
-    Livro.categoria == Livros[i].Categoria &&
-    Livro.Preco == Livros[i].Preco
-      ? true
-      : false;
-  }
-};
+// let repeticaoLivros = (array, value) => {
+//   let number = array.reduce((contador, item) => {
+//     value.Nome == item.Nome &&
+//     value.Ano == item.Ano &&
+//     value.Categoria == item.Categoria &&
+//     value.Preco == item.Preco
+//       ? contador + 1
+//       : contador;
+//   }, 0);
+//   return number;
+// };
